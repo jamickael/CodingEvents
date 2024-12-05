d39f683a0a03092e147f9fb98663b16aeaa04637
Julie <jamickael@gmail.com>
2024-12-04 21:21:15 -0600

    follow along not finished bc error No Static Resource RetrieveResource Found Exception

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,24 +1,20 @@
 package org.launchcode.codingevents.controllers;
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
+    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        ArrayList<String> events = new ArrayList<>();
-        events.add("EventOne");
-        events.add("EventTwo");
-        events.add("EventThree");
-        events.add("EventFour");
-        //model.addAttribute("events", events);
+        model.addAttribute("events", events);
         return "events/index";
     }
 
@@ -27,6 +23,10 @@ public class EventController {
         return "events/create";
     }
 
-
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+    }
 
 }
